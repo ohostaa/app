829f849528dacaa2ded005db724c2fd8ea33a5c2
ohostaa <168881515+ohostaa@users.noreply.github.com>
2025-07-11 21:37:25 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-// src/index.js
-const express = require('express');
-const path = require('path');
+// src/index.mjs
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.set('view engine', 'ejs');
